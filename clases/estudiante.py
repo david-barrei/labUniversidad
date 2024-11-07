7e45843b59ca0fbbde34fa8a6a66382469a89ce2
david-barrei <davbarreiro4@gmail.com>
2024-11-06 23:41:43 -0500

    creando las clases

---
+++ clases/estudiante.py
@@ -0,0 +1,13 @@
+
+
+
+class Estudiante():
+
+    def __init__(self, carnet, carrera):
+        self.carnet = carnet
+        self.carrera = carrera
+
+    def estudiante(self):
+
+        return (f"El carnet es {self.carnet} la carrera es {self.carrera}")
+    
---
+++ clases/persona.py
@@ -0,0 +1,14 @@
+
+
+class Personas():
+
+    def __init__(self,nombre,edad,sexo):
+        self.nombre = nombre
+        self.edad = edad
+        self.sexo = sexo
+
+    def personas(self):
+
+        return (f"El nombre es {self.nombre} su edad {self.edad} y el genero {self.sexo}")
+       
+
