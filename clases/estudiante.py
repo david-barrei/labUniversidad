06bc806255828c18908264da3d59f2af677ed4eb
david-barrei <davbarreiro4@gmail.com>
2024-11-06 23:51:18 -0500

    Agregando clases de estudiante y profesor

--- clases/estudiante.py
+++ clases/estudiante.py
@@ -11,3 +11,13 @@ class Estudiante():
 
         return (f"El carnet es {self.carnet} la carrera es {self.carrera}")
     
+class Curso():
+
+    def __init__(self,nombre,codigo, profesor):
+        self.nombre = nombre
+        self.codigo = codigo
+        self.profesor = profesor
+
+    def curso(self):
+
+        return (f"El nombre es {self.nombre} el codigo es {self.codigo} y el profesor es {self.profesor}") 
---
+++ clases/profesor.py
@@ -0,0 +1,13 @@
+
+class Profesor():
+
+    def __init__(self,codigo, especialidad):
+        self.codigo = codigo
+        self.especialidad = especialidad
+
+    def profesor(self):
+
+        return (f"El codigo es {self.codigo} la especialidad es {self.especialidad}")
+        
+
+
